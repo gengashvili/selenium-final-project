cae25e008f1a0cf9b4d3179356ba4f312754be4b
giorgi gengashvili <giorgi.gengashvili0@gmail.com>
2023-08-25 23:47:02 +0400

    choose vacant place, register for a new account with wrong email and check if errorMessage appear as excpected

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -32,93 +33,130 @@ public class Main {
         this.actions = new Actions(driver);
     }
 
+
     @Test
     public void testSwoop() {
-        driver.get("https://www.swoop.ge/");
+            driver.get("https://www.swoop.ge/");
 
-        WebElement movieLink = driver.findElement(By.linkText("კინო"));
-        movieLink.click();
+            WebElement movieLink = driver.findElement(By.linkText("კინო"));
+            movieLink.click();
 
-        List<WebElement> moviesList;
-        WebElement movie;
-        WebElement buyButton;
-        WebElement cinemasContainer = null;
-        List<WebElement> cinemas;
+            List<WebElement> moviesList;
+            WebElement movie;
+            WebElement buyButton;
+            WebElement cinemasContainer = null;
+            List<WebElement> cinemas;
 
 
-        boolean foundCaveaEastPoint = false;
-        int movieIndex = 0;
-        while (!foundCaveaEastPoint) {
-            moviesList = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='movies-deal']")));
-            movie = moviesList.get(movieIndex);
+            boolean foundCaveaEastPoint = false;
+            int movieIndex = 0;
+            while (!foundCaveaEastPoint) {
+                moviesList = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='movies-deal']")));
+                movie = moviesList.get(movieIndex);
 
-            wait.until(ExpectedConditions.visibilityOf(movie));
-            actions.moveToElement(movie).perform();
+                wait.until(ExpectedConditions.visibilityOf(movie));
+                actions.moveToElement(movie).perform();
 
-            buyButton = movie.findElement(By.xpath(".//div[@class='cinema-hover']/a[div[@class='info-cinema-ticket']]"));
-            wait.until(ExpectedConditions.elementToBeClickable(buyButton));
-            buyButton.click();
+                buyButton = movie.findElement(By.xpath(".//div[@class='cinema-hover']/a[div[@class='info-cinema-ticket']]"));
+                wait.until(ExpectedConditions.elementToBeClickable(buyButton));
+                buyButton.click();
 
-            cinemasContainer = driver.findElement(By.xpath("//div[contains(@class, 'all-cinemas')]"));
-            cinemas = cinemasContainer.findElements(By.xpath("./ul[contains(@class, 'cinema-tabs')]/li"));
+                cinemasContainer = driver.findElement(By.xpath("//div[contains(@class, 'all-cinemas')]"));
+                cinemas = cinemasContainer.findElements(By.xpath("./ul[contains(@class, 'cinema-tabs')]/li"));
 
-            for (WebElement cinema: cinemas) {
-                if (Objects.equals(cinema.getText(), "კავეა ისთ ფოინთი")) {
-                    cinema.click();
-                    foundCaveaEastPoint = true;
-                    break;
+                for (WebElement cinema : cinemas) {
+                    if (Objects.equals(cinema.getText(), "კავეა ისთ ფოინთი")) {
+                        cinema.click();
+                        foundCaveaEastPoint = true;
+                        break;
+                    }
+                }
+
+                if (!foundCaveaEastPoint) {
+                    driver.navigate().back();
+                    movieIndex++;
                 }
             }
 
-            if (!foundCaveaEastPoint) {
-                driver.navigate().back();
-                movieIndex++;
+
+            WebElement cookieButton = driver.findElement(By.className("acceptCookie"));
+            cookieButton.click();
+
+            List<WebElement> cinemasOptions = cinemasContainer.findElements(By.xpath("./div[@aria-hidden = 'false']/div/div[@aria-hidden = 'false']"));
+
+            //Check that only ‘კავეა ისთ ფოინთი’ options are returned
+            for (WebElement cinemaOption : cinemasOptions) {
+                String cinemaTitle = cinemaOption.findElement(By.xpath("./a/p[contains(@class, 'cinema-title')]")).getText();
+                Assert.assertEquals(cinemaTitle, "კავეა ისთ ფოინთი");
             }
-        }
+
+            List<WebElement> seanseDates = cinemasContainer.findElements(By.xpath("./div[@aria-hidden = 'false']/div/ul[@role = 'tablist']/li"));
+
+            WebElement lastSeanseDate = seanseDates.get(seanseDates.size() - 1);
+            lastSeanseDate.click();
+
+            // div - ები იცვლება თარიღის ცვლილებასთან ერთად და  lastSeanseDate.click() - შემდეგ სიას განახლება სჭირდება რადგან ვალიდური ელემენტები წამოიღოს
+            cinemasOptions = cinemasContainer.findElements(By.xpath("./div[@aria-hidden = 'false']/div/div[@aria-hidden = 'false']"));
+
+            WebElement lastCinemaOption = cinemasOptions.get(cinemasOptions.size() - 1);
+
+            String movieTitle = driver.findElement(By.xpath("//div[@class = 'movie_first_section']/div[@class = 'info']/p[@class = 'name']")).getText();
+            String cinemaTitle = lastCinemaOption.findElement(By.xpath("./a/p[contains(@class, 'cinema-title')]")).getText();
+            String dateTime = lastSeanseDate.getText();
+
+            lastCinemaOption.click();
+
+            WebElement popUpMovieInfo = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class = 'right-content']/div[@class = 'content-header']")));
+            String popUpMovieTitle = popUpMovieInfo.findElement(By.xpath("./p[@class = 'movie-title']")).getText();
+            String popUpCinemaTitle = popUpMovieInfo.findElement(By.xpath("./p[@class = 'movie-cinema']")).getText();
+            String popUpDateTime = popUpMovieInfo.findElement(By.xpath("./p[@class = 'movie-cinema'][2]")).getText();
+
+            // Check in opened popup that movie name, cinema and datetime is valid
+            Assert.assertEquals(movieTitle, popUpMovieTitle, "movieTitle and popUpMovieTitle are not equal");
+            Assert.assertEquals(cinemaTitle, popUpCinemaTitle, "cinemaTitle and popUpCinemaTitle are not equal");
+            Assert.assertEquals(Helper.splitDate(dateTime), Helper.splitDate(popUpDateTime), "dateTime and popUoDateTime are not equal");
 
 
-        WebElement cookieButton = driver.findElement(By.className("acceptCookie"));
-        cookieButton.click();
+            List<WebElement> vacantPlaces = driver.findElements(By.xpath("//div[@class = 'seat free']"));
+            vacantPlaces.get(0).click();
 
-        List<WebElement> cinemasOptions = cinemasContainer.findElements(By.xpath("./div[@aria-hidden = 'false']/div/div[@aria-hidden = 'false']"));
+            WebElement registerLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("register")));
+            registerLink.click();
 
-        //Check that only ‘კავეა ისთ ფოინთი’ options are returned
-        for (WebElement cinemaOption : cinemasOptions) {
-            String cinemaTitle = cinemaOption.findElement(By.xpath("./a/p[contains(@class, 'cinema-title')]")).getText();
-            Assert.assertEquals(cinemaTitle,"კავეა ისთ ფოინთი");
-        }
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("registertabs")));
 
-        List<WebElement> seanseDates = cinemasContainer.findElements(By.xpath("./div[@aria-hidden = 'false']/div/ul[@role = 'tablist']/li"));
+            WebElement nameInput = driver.findElement(By.id("pFirstName"));
+            WebElement lastNameInput = driver.findElement(By.id("pLastName"));
+            WebElement emailInput = driver.findElement(By.id("pEmail"));
+            WebElement numberInput = driver.findElement(By.id("pPhone"));
+            WebElement birthdayDateInput = driver.findElement(By.id("pDateBirth"));
+            WebElement genderInput = driver.findElement(By.id("pGender"));
+            WebElement passwordInput = driver.findElement(By.id("pPassword"));
+            WebElement confirmPasswordInput = driver.findElement(By.id("pConfirmPassword"));
+            WebElement termsAndConditionsCheckBox = driver.findElement(By.id("pIsAgreeTerns"));
 
-        WebElement lastSeanseDate = seanseDates.get(seanseDates.size() - 1);
-        lastSeanseDate.click();
+            nameInput.sendKeys("giorgi");
+            lastNameInput.sendKeys("gengashvili");
+            emailInput.sendKeys("wrong mail");
+            numberInput.sendKeys("25 60 60"); // ავეჯის გადაზიდვის სერვისი :)
+            birthdayDateInput.sendKeys("08-23-2002");
+            new Select(genderInput).selectByValue("1");
+            passwordInput.sendKeys("password");
+            confirmPasswordInput.sendKeys("password");
+            termsAndConditionsCheckBox.click();
 
-        // div - ები იცვლება თარიღის ცვლილებასთან ერთად და  lastSeanseDate.click() - შემდეგ სიას განახლება სჭირდება რადგან ვალიდური ელემენტები წამოიღოს
-        cinemasOptions = cinemasContainer.findElements(By.xpath("./div[@aria-hidden = 'false']/div/div[@aria-hidden = 'false']"));
+            WebElement registerButton = driver.findElement(By.xpath("//input[@type = 'button' and @value = 'რეგისტრაცია']"));
+            registerButton.click();
 
-        WebElement lastCinemaOption = cinemasOptions.get(cinemasOptions.size() - 1);
+            WebElement errorMessage = driver.findElement(By.id("physicalInfoMassage"));
+            Assert.assertEquals(errorMessage.getText(), "მეილის ფორმატი არასწორია!");
 
-        String movieTitle = driver.findElement(By.xpath("//div[@class = 'movie_first_section']/div[@class = 'info']/p[@class = 'name']")).getText();
-        String cinemaTitle = lastCinemaOption.findElement(By.xpath("./a/p[contains(@class, 'cinema-title')]")).getText();
-        String dateTime = lastSeanseDate.getText();
-
-        lastCinemaOption.click();
-
-        WebElement popUpMovieInfo = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class = 'right-content']/div[@class = 'content-header']")));
-        String popUpMovieTitle = popUpMovieInfo.findElement(By.xpath("./p[@class = 'movie-title']")).getText();
-        String popUpCinemaTitle = popUpMovieInfo.findElement(By.xpath("./p[@class = 'movie-cinema']")).getText();
-        String popUpDateTime = popUpMovieInfo.findElement(By.xpath("./p[@class = 'movie-cinema'][2]")).getText();
-
-        // Check in opened popup that movie name, cinema and datetime is valid
-        Assert.assertEquals(movieTitle,popUpMovieTitle, "movieTitle and popUpMovieTitle are not equal");
-        Assert.assertEquals(cinemaTitle,popUpCinemaTitle, "cinemaTitle and popUpCinemaTitle are not equal");
-        Assert.assertEquals(Helper.splitDate(dateTime),Helper.splitDate(popUpDateTime), "dateTime and popUoDateTime are not equal");
 
     }
 
 
     @AfterMethod
     public void tearDown() {
-        //driver.quit();
+        driver.quit();
     }
 }
